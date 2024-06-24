98a5542936450001cde16c0a2e7ee4e5c9a42e5c
Davi Alves <davi16lins@gmail.com>
2024-06-24 13:20:34 -0300

    Preparação p/Prova

---
+++ API1/src/controllers/CicloController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import CicloSchema from "../models/CicloSchema";
+
+class CicloController {
+  async listar(request: Request, response: Response) {
+    const ciclos = await CicloSchema.find();
+    response.status(200).json(ciclos);
+  }
+
+  async buscarPorId(request: Request, response: Response) {
+    const { id } = request.params;
+    const ciclo = await CicloSchema.find({ _id: id });
+    response.status(200).json(ciclo);
+  }
+
+  async cadastrar(request: Request, response: Response) {
+    try {
+      const novoCiclo = await CicloSchema.create(request.body);
+      response.status(201).json(novoCiclo);
+    } catch (error) {
+      response.status(400).json({
+        objeto: error,
+        msg: "Falha na validação",
+        erro: true
+      });
+    }
+  }
+}
+
+export { CicloController };
---
+++ FRONT1/src/app/components/views/listar/listar.component.ts
@@ -0,0 +1,22 @@
+import { Ciclo } from '../../../models/Ciclo';
+import { CicloService } from "./../../../services/ciclo.service";
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: "app-listar",
+  templateUrl: "./listar.component.html",
+  styleUrls: ["./listar.component.css"],
+})
+export class ListarComponent implements OnInit {
+  ciclos!: MatTableDataSource<Ciclo>;
+  displayedColumns: string[] = ['id', 'data', 'criadoEm'];
+
+  constructor(private service: CicloService) {}
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((ciclos) => {
+      this.ciclos = new MatTableDataSource<Ciclo>(ciclos);
+    });
+  }
+}
